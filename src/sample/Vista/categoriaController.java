51f13fb79bc47b0871f150e20eaff97246da8702
EyssyDev <193286@ids.upchiapas.edu.mx>
2021-04-14 03:01:26 -0500

    Agregue el metodo de actualizar tablas, en categorias

--- src/sample/Vista/categoriaController.java
+++ src/sample/Vista/categoriaController.java
@@ -2,25 +2,40 @@ package sample.Vista;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import sample.Datos.categoriaDAO;
+import sample.Main;
+import sample.Modelo.categoria;
 
-public class categoriaController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private TableView<?> tableView;
-
-    @FXML
-    private TableColumn<?, ?> clasificacionColumn;
-
-    @FXML
-    private TableColumn<?, ?> categoriaColumn;
+public class categoriaController implements Initializable {
 
     @FXML
-    private TableColumn<?, ?> subcatColumn;
+    private TableView<categoria> tableView;
+
+    @FXML
+    private TableColumn<categoria, String> clasificacionColumn;
+
+    @FXML
+    private TableColumn<categoria, String> categoriaColumn;
+
+    @FXML
+    private TableColumn<categoria, String> subcatColumn;
+
+    private ObservableList<categoria> items = FXCollections.observableArrayList();
+    //Separar por ahora
 
     @FXML
     private JFXTextField categoriaTextfield;
@@ -32,17 +47,57 @@ public class categoriaController {
     private JFXButton botonCategorias;
 
     @FXML
-    private ComboBox<String> calsificacionCombo;
-
-    @FXML
-    private JFXButton regresarBoton;
+    private ComboBox calsificacionCombo;
 
     @FXML
     void botonClicked(MouseEvent event) {
+        categoriaDAO dao = new categoriaDAO();
+        categoria categoriaNueva = new categoria();
+        if(tableView.getSelectionModel().getSelectedItem() == null) {
+            //Metodo Agregar
+            categoriaNueva.setNombre(categoriaTextfield.getText());
+            categoriaNueva.setSubcategorias(subTextfield.getText());
+            categoriaNueva.setClasificacion(calsificacionCombo.getSelectionModel().getSelectedItem().toString());
+            dao.agregarCategoria(categoriaNueva);
+        }
+        else {
+            System.out.println("Entro bro.");
+            categoriaNueva.setNombre(categoriaTextfield.getText());
+            categoriaNueva.setSubcategorias(subTextfield.getText());
+            categoriaNueva.setClasificacion(calsificacionCombo.getSelectionModel().getSelectedItem().toString());
 
+            categoria categoriaVieja = tableView.getSelectionModel().getSelectedItem();
+
+            dao.actualizarCategoria(categoriaNueva, categoriaVieja);
+        }
+        items.clear();
+        listar(items);
+        tableView.setItems(items);
+        clasificacionColumn.setCellValueFactory(new PropertyValueFactory<categoria,String>("clasificacion"));
+        categoriaColumn.setCellValueFactory(new PropertyValueFactory<categoria,String>("nombre"));
+        subcatColumn.setCellValueFactory(new PropertyValueFactory<categoria,String>("subcategorias"));
     }
     @FXML
     void regresarClicked(MouseEvent event) {
 
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        listar(items);
+        tableView.setItems(items);
+        clasificacionColumn.setCellValueFactory(new PropertyValueFactory<categoria,String>("clasificacion"));
+        categoriaColumn.setCellValueFactory(new PropertyValueFactory<categoria,String>("nombre"));
+        subcatColumn.setCellValueFactory(new PropertyValueFactory<categoria,String>("subcategorias"));
+
+        ObservableList<String> listaString = FXCollections.observableArrayList("Personal", "Asociado", "Deuda", "Impuesto"); //Nota: Son Strings, por lo que le pueden agregar cualquier cosa. valores por determinados.
+        calsificacionCombo.setItems(listaString);
+    }
+
+    public void listar(ObservableList<categoria> items) {
+        categoriaDAO dao = new categoriaDAO();
+        for (categoria categorias:dao.getListaCategorias()) {
+            System.out.println(categorias);
+            items.add(categorias);
+        }
     }
 }
