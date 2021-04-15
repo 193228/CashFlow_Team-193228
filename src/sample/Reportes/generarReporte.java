8ede855ce4e31465c1f1b13378c595447e701c58
193228 <193228@ids.upchiapas.edu.mx>
2021-04-15 10:45:33 -0500

    necesitamos sanear la clase de flujo de efectivo y reportes

--- src/sample/Reportes/generarReporte.java
+++ src/sample/Reportes/generarReporte.java
@@ -9,17 +9,6 @@ import sample.Adaptador.adaptadorSql;
 public class generarReporte {
 
 
-    public void abrirReporte() {
-        adaptadorSql conector = new adaptadorSql();
-        JasperPrint jasperPrintWindow;
-        try {
-            jasperPrintWindow = JasperFillManager.fillReport(
-                    "Reportes/Reporte.jasper", null,conector.getConnection());
-            JasperExportManager.exportReportToPdfFile(jasperPrintWindow, "Informe/informe.pdf");
-            JasperViewer.viewReport(jasperPrintWindow, false);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    
 
 }
